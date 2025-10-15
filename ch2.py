a6b3447668a9763cbeabf243bb738e7385855087
SangKyu,Choi <guruguru74@gmail.com>
2025-10-15 22:40:00 +0900

    Initial Commit.

---
+++ ch2.py
@@ -0,0 +1,65 @@
+# # 2.1 숫자 자료형
+# print(5)
+# print(-10)
+# print(3.14)
+# print(1000)
+
+# print(5+3)
+# print(2*8)
+# print(6/3)
+# print(3*(3+1))
+
+# # 2.2 문자열 자료형
+# print('풍선')
+# print("나비")
+# print("abcdefg")
+# print("10")
+# print("파이썬" * 3)
+
+# # 2.3 불 자료형
+# print(5 > 10)
+# print(5 < 10)
+
+# print(not True)
+# print(not False)
+
+# print(not (5 > 10))
+
+# 2.4 변수
+# print("반려동물을 소개해 주세요.")
+# print("우리 집 반려동물은 개인데, 이름이 연탄이예요.")
+# print("연탄이는 4살이고, 산책을 아주 좋아해요.")
+# print("연탄이는 수컷인가요")
+# print("네.")
+
+# name = "연탄이"
+# animal = "개"
+# age = 4
+# hobby = "산책"
+# is_male = True
+
+# print("반려동물을 소개해 주세요.")
+# print("우리 집 반려동물은 " + animal + "인데, 이름이 " + name + "이예요.")
+# print(name + "는 " + str(age) + "살이고, " + hobby + "을 아주 좋아해요.")
+
+# print(name, "는", age, "살이고,", hobby, "을 아주 좋아해요.")
+
+# 2.4.3 형변환하기
+print(int("3"))
+print(int(3.5))
+#print(int("삼"))
+print(float("3.5"))
+print(float(3))
+#print(float("오"))
+
+print(str(3) + " 입니다.")
+print(str(3.5) + " 입니다.")
+
+# 2.5 주석
+name2 = "연탄이"  # 반려동물 이름
+
+""" 여러 줄 주석 처리.
+print("반려동물을 소개해 주세요.")
+print("우리 집 반려동물은 " + animal + "인데, 이름이 " + name + "이예요.")
+print(name + "는 " + str(age) + "살이고, " + hobby + "을 아주 좋아해요.")
+"""
